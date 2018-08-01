3fac16db56f50d8e179b7af6de077933b106ef07
Jason Dillon <20114+jdillon@users.noreply.github.com>
2018-08-01 14:41:35 -0700

    Add aop-metrics support, add datadog metrics reporting support (#2)

---
+++ core/src/main/java/org/sonatype/goodies/dropwizard/internal/MetricsAopModule.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.internal;
+
+import java.lang.reflect.Method;
+
+import javax.annotation.Nonnull;
+
+import com.codahale.metrics.annotation.Counted;
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Gauge;
+import com.codahale.metrics.annotation.Metered;
+import com.codahale.metrics.annotation.Timed;
+import com.google.inject.AbstractModule;
+import com.palominolabs.metrics.guice.MetricNamer;
+import com.palominolabs.metrics.guice.MetricsInstrumentationModule;
+import io.dropwizard.setup.Environment;
+
+import static com.codahale.metrics.MetricRegistry.name;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Adds support for Guice-AOP based metrics.
+ *
+ * @since ???
+ */
+public class MetricsAopModule
+    extends AbstractModule
+{
+  private final Environment environment;
+
+  public MetricsAopModule(final Environment environment) {
+    this.environment = checkNotNull(environment);
+  }
+
+  @Override
+  protected void configure() {
+    install(MetricsInstrumentationModule.builder()
+        .withMetricRegistry(environment.metrics())
+        .withMetricNamer(new MetricNamerImpl())
+        .build());
+  }
+
+  /**
+   * Customized metric naming; default appends suffixes which are non-standard with other DW metrics.
+   */
+  @SuppressWarnings("Duplicates")
+  private static class MetricNamerImpl
+      implements MetricNamer
+  {
+    @Nonnull
+    @Override
+    public String getNameForCounted(@Nonnull final Method method, @Nonnull final Counted counted) {
+      if (counted.absolute()) {
+        return counted.name();
+      }
+      else if (counted.name().isEmpty()) {
+        return name(method.getDeclaringClass(), method.getName());
+      }
+      else {
+        return name(method.getDeclaringClass(), counted.name());
+      }
+    }
+
+    @Nonnull
+    @Override
+    public String getNameForExceptionMetered(@Nonnull final Method method,
+                                             @Nonnull final ExceptionMetered exceptionMetered)
+    {
+      if (exceptionMetered.absolute()) {
+        return exceptionMetered.name();
+      }
+      else if (exceptionMetered.name().isEmpty()) {
+        return
+            name(method.getDeclaringClass(), method.getName());
+      }
+      else {
+        return name(method.getDeclaringClass(), exceptionMetered.name());
+      }
+    }
+
+    @Nonnull
+    @Override
+    public String getNameForGauge(@Nonnull final Class<?> instanceClass,
+                                  @Nonnull final Method method,
+                                  @Nonnull final Gauge gauge)
+    {
+      if (gauge.absolute()) {
+        return gauge.name();
+      }
+      else if (gauge.name().isEmpty()) {
+        return name(method.getDeclaringClass(), method.getName());
+      }
+      else {
+        return name(method.getDeclaringClass(), gauge.name());
+      }
+    }
+
+    @Nonnull
+    @Override
+    public String getNameForMetered(@Nonnull final Method method, @Nonnull final Metered metered) {
+      if (metered.absolute()) {
+        return metered.name();
+      }
+      else if (metered.name().isEmpty()) {
+        return name(method.getDeclaringClass(), method.getName());
+      }
+      else {
+        return name(method.getDeclaringClass(), metered.name());
+      }
+    }
+
+    @Nonnull
+    @Override
+    public String getNameForTimed(@Nonnull final Method method, @Nonnull final Timed timed) {
+      if (timed.absolute()) {
+        return timed.name();
+      }
+      else if (timed.name().isEmpty()) {
+        return name(method.getDeclaringClass(), method.getName());
+      }
+      else {
+        return name(method.getDeclaringClass(), timed.name());
+      }
+    }
+  }
+}
---
+++ datadog/src/main/java/org/sonatype/goodies/dropwizard/datadog/MetricNameFormatterImpl.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.datadog;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.coursera.metrics.datadog.DefaultMetricNameFormatter;
+import org.coursera.metrics.datadog.MetricNameFormatter;
+import org.coursera.metrics.datadog.MetricNameFormatterFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Custom Datadog metric-name formatter.
+ *
+ * @since ???
+ */
+public class MetricNameFormatterImpl
+    extends DefaultMetricNameFormatter
+{
+  private static final Logger log = LoggerFactory.getLogger(MetricNameFormatterImpl.class);
+
+  @Nullable
+  private final String prefix;
+
+  @Nullable
+  private final Map<String,String> replacements;
+
+  public MetricNameFormatterImpl(@Nullable final String prefix, @Nullable final Map<String,String> replacements) {
+    this.prefix = prefix;
+    log.debug("Prefix: {}", prefix);
+
+    this.replacements = replacements;
+    log.debug("Replacements: {}", replacements);
+  }
+
+  @Override
+  public String format(final String name, final String... path) {
+    StringBuilder buff = new StringBuilder(name);
+
+    if (replacements != null) {
+      for (Map.Entry<String,String> entry : replacements.entrySet()) {
+        String from = entry.getKey();
+        int i = buff.indexOf(from);
+        if (i != -1) {
+          String to = entry.getValue();
+          buff.replace(i, i + from.length(), to);
+        }
+      }
+    }
+
+    if (prefix != null) {
+      buff.insert(0, prefix);
+    }
+
+    String result = super.format(buff.toString(), path);
+
+    if (log.isTraceEnabled()) {
+      log.trace("Format; name={}, path={} -> {}", name, Arrays.asList(path), result);
+    }
+
+    return result;
+  }
+
+  //
+  // Factory
+  //
+
+  @JsonTypeName("custom")
+  public static class Factory
+      implements MetricNameFormatterFactory
+  {
+    @Nullable
+    @JsonProperty
+    private String prefix;
+
+    @Nullable
+    @JsonProperty
+    private Map<String,String> replacements;
+
+    @Nullable
+    public String getPrefix() {
+      return prefix;
+    }
+
+    public void setPrefix(@Nullable final String prefix) {
+      this.prefix = prefix;
+    }
+
+    @Nullable
+    public Map<String, String> getReplacements() {
+      return replacements;
+    }
+
+    public void setReplacements(@Nullable final Map<String, String> replacements) {
+      this.replacements = replacements;
+    }
+
+    @Override
+    public MetricNameFormatter build() {
+      return new MetricNameFormatterImpl(prefix, replacements);
+    }
+  }
+}
