9d5a7829acb04fe64ab66893e9bd8c596068b028
Jason Dillon <20114+jdillon@users.noreply.github.com>
2018-08-08 12:14:52 -0700

    Metrics refinement (#3)
    * explicitly call out that replacements needs to be LinkedHashMap; which it appears to already be
    * Add more customized metric-name generation with formatting
    * Rename for clarity; add default replacements, use factory as config
    * DW unfortunatly has uncontrollable metrics for REST endpoints, which contradicts the guice-aop metrics; so we have to use absolute naming for REST endpoints
    * disable fallbacks for now, lets see whats actually needed first

---
+++ core/src/main/java/org/sonatype/goodies/dropwizard/metrics/MetricNamerImpl.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.metrics;
+
+import java.lang.reflect.Method;
+
+import javax.annotation.Nonnull;
+
+import com.codahale.metrics.annotation.Counted;
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Gauge;
+import com.codahale.metrics.annotation.Metered;
+import com.codahale.metrics.annotation.Timed;
+import com.palominolabs.metrics.guice.MetricNamer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.codahale.metrics.MetricRegistry.name;
+
+/**
+ * Customized metric naming.
+ *
+ * @since ???
+ * @see MetricNameFormat
+ */
+public class MetricNamerImpl
+    implements MetricNamer
+{
+  private static final Logger log = LoggerFactory.getLogger(MetricNamerImpl.class);
+
+  /**
+   * Replaces with class-name.
+   */
+  private static final String CLASS = "#class";
+
+  /**
+   * Replaces with simple class-name.
+   */
+  private static final String SIMPLE_CLASS = "#simpleClass";
+
+  /**
+   * Replaces with method-name.
+   */
+  private static final String METHOD = "#method";
+
+  /**
+   * Replaces with customized name, or if not present method-name.
+   */
+  public static final String NAME = "#name";
+
+  /**
+   * Generate metric name.
+   */
+  private String metricName(final Method method, final String name) {
+    String result;
+    Class<?> type = method.getDeclaringClass();
+
+    // apply customized formatting if annotation is present
+    MetricNameFormat format = type.getAnnotation(MetricNameFormat.class);
+    if (format != null) {
+      result = format.value()
+          .replace(CLASS, type.getName())
+          .replace(SIMPLE_CLASS, type.getSimpleName())
+          .replace(METHOD, method.getName())
+          .replace(NAME, name.isEmpty() ? method.getName() : name);
+    }
+    else if (name.isEmpty()) {
+      result = name(type.getName(), method.getName());
+    }
+    else {
+      result = name(type.getName(), name);
+    }
+
+    if (log.isTraceEnabled()) {
+      log.trace("Metric-name: {}:{}:{} -> {}", type.getName(), method.getName(), name, result);
+    }
+
+    return result;
+  }
+
+  @Nonnull
+  @Override
+  public String getNameForCounted(@Nonnull final Method method, @Nonnull final Counted counted) {
+    if (counted.absolute()) {
+      return counted.name();
+    }
+    else {
+      return metricName(method, counted.name());
+    }
+  }
+
+  @Nonnull
+  @Override
+  public String getNameForExceptionMetered(@Nonnull final Method method,
+                                           @Nonnull final ExceptionMetered exceptionMetered)
+  {
+    if (exceptionMetered.absolute()) {
+      return exceptionMetered.name();
+    }
+    else {
+      return metricName(method, exceptionMetered.name());
+    }
+  }
+
+  @Nonnull
+  @Override
+  public String getNameForGauge(@Nonnull final Class<?> instanceClass,
+                                @Nonnull final Method method,
+                                @Nonnull final Gauge gauge)
+  {
+    if (gauge.absolute()) {
+      return gauge.name();
+    }
+    else {
+      return metricName(method, gauge.name());
+    }
+  }
+
+  @Nonnull
+  @Override
+  public String getNameForMetered(@Nonnull final Method method, @Nonnull final Metered metered) {
+    if (metered.absolute()) {
+      return metered.name();
+    }
+    else {
+      return metricName(method, metered.name());
+    }
+  }
+
+  @Nonnull
+  @Override
+  public String getNameForTimed(@Nonnull final Method method, @Nonnull final Timed timed) {
+    if (timed.absolute()) {
+      return timed.name();
+    }
+    else {
+      return metricName(method, timed.name());
+    }
+  }
+}
---
+++ core/src/main/java/org/sonatype/goodies/dropwizard/metrics/MetricsAopModule.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.metrics;
+
+import com.google.inject.AbstractModule;
+import com.palominolabs.metrics.guice.MetricsInstrumentationModule;
+import io.dropwizard.setup.Environment;
+
+import static com.codahale.metrics.MetricRegistry.name;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Adds support for Guice-AOP based metrics.
+ *
+ * @since ???
+ */
+public class MetricsAopModule
+    extends AbstractModule
+{
+  private final Environment environment;
+
+  public MetricsAopModule(final Environment environment) {
+    this.environment = checkNotNull(environment);
+  }
+
+  @Override
+  protected void configure() {
+    install(MetricsInstrumentationModule.builder()
+        .withMetricRegistry(environment.metrics())
+        .withMetricNamer(new MetricNamerImpl())
+        .build());
+  }
+}
