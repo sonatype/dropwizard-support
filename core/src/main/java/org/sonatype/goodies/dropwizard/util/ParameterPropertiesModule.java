4ad9e5c8da96f3c469abd6a24983e2096561c2e5
Jason Dillon <jdillon@users.noreply.github.com>
2018-08-21 18:38:25 -0700

    Add ParameterPropertiesModule helper

---
+++ core/src/main/java/org/sonatype/goodies/dropwizard/util/ParameterPropertiesModule.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.util;
+
+import java.util.Map;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import org.eclipse.sisu.wire.ParameterKeys;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Binds Sisu {@link ParameterKeys#PROPERTIES}.
+ *
+ * Properties are used in {@link javax.inject.Named} annotations with {@code ${name:-default}} syntax.
+ *
+ * @since ???
+ */
+public class ParameterPropertiesModule
+    implements Module
+{
+  private final Map<String,Object> properties;
+
+  public ParameterPropertiesModule(final Map<String, Object> properties) {
+    this.properties = checkNotNull(properties);
+  }
+
+  @Override
+  public void configure(final Binder binder) {
+    binder.bind(ParameterKeys.PROPERTIES).toInstance(properties);
+  }
+}
