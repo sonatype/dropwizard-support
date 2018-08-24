bf95a8f3a48bb7d4305664e66feedb3d7f91eea6
Jason Dillon <jdillon@users.noreply.github.com>
2018-08-24 12:42:07 -0700

    Add NOT string-matcher

---
+++ rules/src/main/java/org/sonatype/goodies/dropwizard/rules/matcher/string/NotStringMatcher.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.rules.matcher.string;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * NOT {@link StringMatcher}.
+ *
+ * @since ???
+ */
+@JsonTypeName(NotStringMatcher.TYPE)
+public class NotStringMatcher
+    implements StringMatcher
+{
+  public static final String TYPE = "not";
+
+  private final StringMatcher matcher;
+
+  public NotStringMatcher(@NotNull @JsonProperty("matcher") final StringMatcher matcher) {
+    this.matcher = checkNotNull(matcher);
+  }
+
+  @Override
+  public boolean matches(final String string) {
+    return !matcher.matches(string);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s{%s}", TYPE, matcher);
+  }
+}
