533d409c0b1666ea242895a55215628206f5a170
Jason Dillon <jdillon@users.noreply.github.com>
2018-08-23 17:32:43 -0700

    tidy

--- rules/src/main/java/org/sonatype/goodies/dropwizard/rules/RequestRuleServiceImpl.java
+++ rules/src/main/java/org/sonatype/goodies/dropwizard/rules/RequestRuleServiceImpl.java
@@ -46,8 +46,6 @@ public class RequestRuleServiceImpl
   @Inject
   public RequestRuleServiceImpl(final RequestRuleConfiguration config) {
     this.config = checkNotNull(config);
-
-    log.info("Rules: {}", config.getRules());
   }
 
   @Override
