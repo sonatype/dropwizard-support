3e0ddfbcf662c3bf1b6dc821372b6e5310c14352
Jason Dillon <20114+jdillon@users.noreply.github.com>
2019-02-06 16:15:36 -0800

    JDBI module (#12)
    Add JDBI support module

---
+++ jdbi/src/main/java/org/sonatype/goodies/dropwizard/jdbi/OnDemandDaoProviderSupport.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.jdbi;
+
+import javax.inject.Provider;
+
+import org.jdbi.v3.core.Jdbi;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Helper to provide JDBI <em>on-demand</em> DAO instances.
+ *
+ * @since ???
+ */
+public class OnDemandDaoProviderSupport<T>
+    implements Provider<T>
+{
+  private final Jdbi database;
+
+  private final Class<T> type;
+
+  public OnDemandDaoProviderSupport(final Jdbi database, final Class<T> type) {
+    this.database = checkNotNull(database);
+    checkArgument(type.isInterface(), "Type not interface: %s", type);
+    this.type = checkNotNull(type);
+  }
+
+  @Override
+  public T get() {
+    return database.onDemand(type);
+  }
+}
---
+++ jdbi/src/main/java/org/sonatype/goodies/dropwizard/jdbi/Slf4jSqlLogger.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.jdbi;
+
+import java.sql.SQLException;
+
+import org.sonatype.goodies.dropwizard.util.Level;
+
+import org.jdbi.v3.core.statement.SqlLogger;
+import org.jdbi.v3.core.statement.StatementContext;
+import org.slf4j.Logger;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Slf4j {@link SqlLogger} adapter.
+ *
+ * @since ???
+ */
+public class Slf4jSqlLogger
+    implements SqlLogger
+{
+  private final Logger logger;
+
+  private final Level level;
+
+  public Slf4jSqlLogger(final Logger logger, final Level level) {
+    this.logger = checkNotNull(logger);
+    this.level = checkNotNull(level);
+  }
+
+  @Override
+  public void logBeforeExecution(final StatementContext context) {
+    level.log(logger, "Before-execution; SQL: {}, Parsed: {}; Attributes: {}",
+        context.getRenderedSql(),
+        context.getParsedSql(),
+        context.getAttributes()
+    );
+  }
+
+  @Override
+  public void logException(final StatementContext context, final SQLException e) {
+    logger.warn("Exception", e);
+  }
+}
