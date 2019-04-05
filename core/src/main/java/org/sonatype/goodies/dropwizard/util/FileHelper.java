13995fcc3295a21d04cc0280912a68ae9e7a8e04
Jason Dillon <jdillon@users.noreply.github.com>
2019-04-05 13:55:36 -0700

    Add additional file helpers

--- core/src/main/java/org/sonatype/goodies/dropwizard/util/FileHelper.java
+++ core/src/main/java/org/sonatype/goodies/dropwizard/util/FileHelper.java
@@ -14,6 +14,12 @@ package org.sonatype.goodies.dropwizard.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -24,16 +30,96 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public class FileHelper
 {
-  public static File resolveFile(final String path) {
-    checkNotNull(path);
+  private static final Logger log = LoggerFactory.getLogger(FileHelper.class);
+
+  /**
+   * Return canonical file; rethrowing any exceptions as unchecked.
+   *
+   * @since 1.0.4
+   */
+  public static File canonical(final File file) {
+    checkNotNull(file);
     try {
-      return new File(path).getCanonicalFile();
+      return file.getCanonicalFile();
     }
     catch (IOException e) {
+      log.warn("Failed to canonicalize file: {}", file, e);
+
+      // rethrow
       throw new RuntimeException(e);
     }
   }
 
+  /**
+   * Copy source file to target file.
+   *
+   * Will replace existing target file and ensure target directory structure.
+   *
+   * @since 1.0.4
+   */
+  public static void copy(final File source, final File target) throws IOException {
+    checkNotNull(source);
+    checkNotNull(target);
+
+    log.trace("Copy: {} -> {}", source, target);
+    Files.createDirectories(target.getParentFile().toPath());
+    Files.copy(source.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+  }
+
+  /**
+   * Extract filename from given path.
+   *
+   * @since 1.0.4
+   */
+  public static String filename(final String path) {
+    checkNotNull(path);
+    return Paths.get(path).getFileName().toString();
+  }
+
+  /**
+   * Extract file extension from given path; sans-leading {@literal .}.
+   *
+   * @since 1.0.4
+   */
+  public static String extension(final String path) {
+    checkNotNull(path);
+    int i = path.lastIndexOf('.');
+    if (i == -1) {
+      return "";
+    }
+    return path.substring(i + 1);
+  }
+
+  /**
+   * Attempt to delete given file.
+   *
+   * @since 1.0.4
+   */
+  public static void delete(final File file) {
+    checkNotNull(file);
+    if (file.exists()) {
+      try {
+        log.trace("Delete: {}", file);
+        Files.delete(file.toPath());
+      }
+      catch (IOException e) {
+        log.warn("Failed to delete file: {}", file, e);
+        // gobble exception
+      }
+    }
+  }
+
+  /**
+   * Resolve file for given path.
+   */
+  public static File resolveFile(final String path) {
+    checkNotNull(path);
+    return canonical(new File(path));
+  }
+
+  /**
+   * Resolve path.
+   */
   public static String resolvePath(final String path) {
     return resolveFile(path).getPath();
   }
