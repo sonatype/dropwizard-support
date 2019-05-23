8c7d1a62854cd6db1591f725f1fe125ee9760b22
Jason Dillon <jdillon@users.noreply.github.com>
2019-05-23 14:02:09 -0700

    include more boilerplate framework to expose version details

---
+++ core/src/main/java/org/sonatype/goodies/dropwizard/version/VersionResourceSupport.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.version;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+import org.sonatype.goodies.dropwizard.app.ApplicationVersion;
+import org.sonatype.goodies.dropwizard.jaxrs.ResourceSupport;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+/**
+ * Support for {@link Version} resources.
+ *
+ * @since ???
+ */
+@Api(value = "Version")
+public abstract class VersionResourceSupport
+    extends ResourceSupport
+{
+  private final ApplicationVersion version;
+
+  @Inject
+  public VersionResourceSupport(final ApplicationVersion version) {
+    this.version = checkNotNull(version);
+  }
+
+  @GET
+  @Produces({APPLICATION_JSON})
+  @ApiOperation(value = "Get version information")
+  @ApiResponses({
+      @ApiResponse(code = 200, message = "Version information")
+  })
+  public Version get() {
+    return new Version(
+        version.getVersion(),
+        version.getBuildTimestamp(),
+        version.getBuildTag(),
+        version.getBuildNotes()
+    );
+  }
+}
---
+++ core/src/test/java/org/sonatype/goodies/dropwizard/version/VersionLoaderTest.groovy
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.version
+
+import com.google.common.io.Resources
+import org.junit.Test
+
+/**
+ * {@link VersionLoader} tests.
+ */
+class VersionLoaderTest
+{
+  @Test
+  void 'properties from URL'() {
+    def underTest = new VersionLoader(Resources.getResource(getClass(), VersionLoader.RESOURCE))
+    assert underTest.version == 'foo'
+    assert underTest.timestamp == 'bar'
+    assert underTest.tag == 'baz'
+    assert underTest.notes == 'qux'
+  }
+
+  @Test
+  void 'properties from resource'() {
+    def underTest = new VersionLoader(getClass())
+    assert underTest.version == 'foo'
+    assert underTest.timestamp == 'bar'
+    assert underTest.tag == 'baz'
+    assert underTest.notes == 'qux'
+  }
+
+  @Test
+  void 'properties missing are unknown'() {
+    def props = new Properties()
+    def underTest = new VersionLoader() {
+      @Override
+      protected Properties load() {
+        return props
+      }
+    }
+    assert underTest.version == VersionLoader.UNKNOWN
+    assert underTest.timestamp == VersionLoader.UNKNOWN
+    assert underTest.tag == VersionLoader.UNKNOWN
+    assert underTest.notes == VersionLoader.UNKNOWN
+  }
+
+  @Test
+  void 'properties non-resolved are unknown'() {
+    def props = new Properties()
+    props.setProperty(VersionLoader.VERSION, '${foo}')
+    props.setProperty(VersionLoader.TIMESTAMP, '${bar}')
+    props.setProperty(VersionLoader.TAG, '${baz}')
+    props.setProperty(VersionLoader.NOTES, '${qux}')
+
+    def underTest = new VersionLoader() {
+      @Override
+      protected Properties load() {
+        return props
+      }
+    }
+    assert underTest.version == VersionLoader.UNKNOWN
+    assert underTest.timestamp == VersionLoader.UNKNOWN
+    assert underTest.tag == VersionLoader.UNKNOWN
+    assert underTest.notes == VersionLoader.UNKNOWN
+  }
+}
