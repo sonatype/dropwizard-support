710e0ed744268583c4974b0d81bba3149c9aca61
Jason Dillon <jdillon@users.noreply.github.com>
2019-10-22 15:33:18 -0700

    * include sts * make s3 and kms optional * add regionhelper

---
+++ aws/src/main/java/org/sonatype/goodies/dropwizard/aws/RegionHelper.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.aws;
+
+import javax.annotation.Nullable;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.util.EC2MetadataUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * AWS region helpers.
+ *
+ * @since ???
+ */
+public class RegionHelper
+{
+  private static final Logger log = LoggerFactory.getLogger(RegionHelper.class);
+
+  private RegionHelper() {
+    // empty
+  }
+
+  @Nullable
+  public static Regions getCurrent() {
+    Regions region = null;
+    String name = EC2MetadataUtils.getEC2InstanceRegion();
+    if (name != null) {
+      region = Regions.fromName(name);
+    }
+    log.trace("Current region: {}", region);
+    return region;
+  }
+}
