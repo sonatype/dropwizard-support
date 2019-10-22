e907a755d4f7f3649d833126e71f5bf8f2bee7a0
Jason Dillon <20114+jdillon@users.noreply.github.com>
2019-10-22 12:51:43 -0700

    Add local-realm; move empty-realm to realms package (#30)

---
+++ security/src/main/java/org/sonatype/goodies/dropwizard/security/realms/local/LocalRealm.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.security.realms.local;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.apache.shiro.authc.SimpleAccount;
+import org.apache.shiro.authz.SimpleRole;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.apache.shiro.util.PermissionUtils.resolvePermissions;
+
+/**
+ * Local {@link SimpleAccountRealm realm}.
+ *
+ * @since ???
+ */
+@Named(LocalRealm.NAME)
+@Singleton
+public class LocalRealm
+    extends SimpleAccountRealm
+{
+  private static final Logger log = LoggerFactory.getLogger(LocalRealm.class);
+
+  public static final String NAME = "local";
+
+  @Inject
+  public LocalRealm(final LocalRealmConfiguration configuration) {
+    setName(NAME);
+
+    checkNotNull(configuration);
+    configure(configuration);
+  }
+
+  private void configure(final LocalRealmConfiguration configuration) {
+    for (LocalRole entry : configuration.getRoles()) {
+      log.debug("Add role: {}", entry);
+
+      // skip duplicate roles
+      SimpleRole role = getRole(entry.getName());
+      if (role != null) {
+        log.warn("Duplicate role: {}", entry);
+        continue;
+      }
+
+      role = new SimpleRole(entry.getName());
+      role.setPermissions(resolvePermissions(entry.getPermissions(), getPermissionResolver()));
+
+      add(role);
+    }
+
+    for (LocalUser entry : configuration.getUsers()) {
+      log.debug("Add user: {}", entry);
+
+      // skip duplicate users
+      SimpleAccount account = getUser(entry.getName());
+      if (account != null) {
+        log.warn("Duplicate user: {}", entry);
+        continue;
+      }
+
+      account = new SimpleAccount(entry.getName(), entry.getPassword(), NAME);
+
+      for (String roleName : entry.getRoles()) {
+        SimpleRole role = getRole(roleName);
+        if (role != null) {
+          account.addRole(role.getName());
+          account.addObjectPermissions(role.getPermissions());
+        }
+      }
+
+      account.addObjectPermissions(resolvePermissions(entry.getPermissions(), getPermissionResolver()));
+
+      add(account);
+    }
+  }
+}
---
+++ security/src/main/java/org/sonatype/goodies/dropwizard/security/realms/local/LocalRealmConfiguration.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.dropwizard.security.realms.local;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * {@link LocalRealm} configuration.
+ *
+ * @since ???
+ */
+public class LocalRealmConfiguration
+{
+  @Valid
+  @NotNull
+  @JsonProperty
+  private List<LocalUser> users = new ArrayList<>();
+
+  @Nonnull
+  public List<LocalUser> getUsers() {
+    return users;
+  }
+
+  public void setUsers(@Nonnull final List<LocalUser> users) {
+    this.users = checkNotNull(users);
+  }
+
+  @Valid
+  @NotNull
+  @JsonProperty
+  private List<LocalRole> roles = new ArrayList<>();
+
+  @Nonnull
+  public List<LocalRole> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(@Nonnull final List<LocalRole> roles) {
+    this.roles = checkNotNull(roles);
+  }
+}
