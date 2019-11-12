eace47d74410e963dae0259bcc3ea4ac95cef053
Jason Dillon <jdillon@users.noreply.github.com>
2019-11-12 11:31:21 -0800

    Replace CamelContextFactory with CamelContextBuilder

--- camel/src/main/java/org/sonatype/goodies/dropwizard/camel/CamelContextBuilder.java
+++ camel/src/main/java/org/sonatype/goodies/dropwizard/camel/CamelContextBuilder.java
@@ -12,13 +12,17 @@
  */
 package org.sonatype.goodies.dropwizard.camel;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.inject.Singleton;
 
 import org.sonatype.goodies.dropwizard.camel.metrics.ServiceMetricsRoutePolicyFactory;
 
 import com.codahale.metrics.MetricRegistry;
+import org.apache.camel.CamelContext;
 import org.apache.camel.component.metrics.MetricsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.spi.ExecutorServiceManager;
@@ -27,32 +31,62 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 
 /**
- * Default {@link org.apache.camel.CamelContext} factory.
+ * {@link CamelContext} builder.
  *
  * @since ???
  */
 @Named
-@Singleton
-public class CamelContextFactory
+public class CamelContextBuilder
 {
-  private static final Logger log = LoggerFactory.getLogger(CamelContextFactory.class);
+  private static final Logger log = LoggerFactory.getLogger(CamelContextBuilder.class);
 
   private final MetricRegistry metricRegistry;
 
+  private String name;
+
+  @Nullable
+  private Map<String,Object> binding;
+
   @Inject
-  public CamelContextFactory(final MetricRegistry metricRegistry) {
+  public CamelContextBuilder(final MetricRegistry metricRegistry) {
     this.metricRegistry = checkNotNull(metricRegistry);
   }
 
-  public DefaultCamelContext create(final String name) {
+  public CamelContextBuilder name(final String name) {
+    this.name = checkNotNull(name);
+    return this;
+  }
+
+  public CamelContextBuilder bind(final String id, final Object value) {
+    checkNotNull(id);
+    checkNotNull(value);
+    if (binding == null) {
+      binding = new HashMap<>();
+    }
+    binding.put(id, value);
+    return this;
+  }
+
+  public CamelContextBuilder logger(final Logger logger) {
+    bind("logger", logger);
+    return this;
+  }
+
+  public DefaultCamelContext build() {
+    checkState(name != null, "Missing: name");
+
     log.debug("Create camel-context: {}", name);
     DefaultCamelContext camelContext = new DefaultCamelContext();
     camelContext.setName(name);
 
     // install standard components into registry
     Registry registry = camelContext.getRegistry();
+    if (binding != null) {
+      binding.forEach(registry::bind);
+    }
     registry.bind(MetricsComponent.METRIC_REGISTRY_NAME, metricRegistry);
 
     // adjust thread-name pattern, a bit less verbose than the defaults
