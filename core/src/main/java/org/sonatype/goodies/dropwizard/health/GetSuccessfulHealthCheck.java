d3819ca291e800c338fc96722b5879da5e4129c5
Jason Dillon <jdillon@users.noreply.github.com>
2020-11-16 12:09:43 -0800

    Fix header

--- core/src/main/java/org/sonatype/goodies/dropwizard/health/GetSuccessfulHealthCheck.java
+++ core/src/main/java/org/sonatype/goodies/dropwizard/health/GetSuccessfulHealthCheck.java
@@ -1,5 +1,4 @@
 /*
-<<<<<<< HEAD
  * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0,
@@ -10,10 +9,6 @@
  * software distributed under the Apache License Version 2.0 is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
-=======
- * Copyright (c) 2019-present Sonatype, Inc. All rights reserved.
- * "Sonatype" is a trademark of Sonatype, Inc.
->>>>>>> master
  */
 package org.sonatype.goodies.dropwizard.health;
 
