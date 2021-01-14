399aa7785b0795ab602355f7205933c1bbf7b372
Jason Dillon <jdillon@users.noreply.github.com>
2021-01-14 13:16:32 -0800

    Revert "explain exception to include better compression on line with level/logger/etc"
    This reverts commit 37aaac7e

--- events/src/main/java/org/sonatype/goodies/dropwizard/events/LoggingSubscriberExceptionHandler.java
+++ events/src/main/java/org/sonatype/goodies/dropwizard/events/LoggingSubscriberExceptionHandler.java
@@ -12,8 +12,6 @@
  */
 package org.sonatype.goodies.dropwizard.events;
 
-import org.sonatype.goodies.dropwizard.util.ThrowableHelper;
-
 import com.google.common.eventbus.SubscriberExceptionContext;
 import com.google.common.eventbus.SubscriberExceptionHandler;
 import org.slf4j.Logger;
@@ -37,13 +35,8 @@ public class LoggingSubscriberExceptionHandler
   }
 
   @Override
-  public void handleException(final Throwable cause, final SubscriberExceptionContext context) {
-    logger.error("Could not dispatch event {} to subscriber {} method [{}]: {}",
-        context.getEvent(),
-        context.getSubscriber(),
-        context.getSubscriberMethod(),
-        ThrowableHelper.explain(cause),
-        cause
-    );
+  public void handleException(final Throwable exception, final SubscriberExceptionContext context) {
+    logger.error("Could not dispatch event {} to subscriber {} method [{}]",
+        context.getEvent(), context.getSubscriber(), context.getSubscriberMethod(), exception);
   }
 }
