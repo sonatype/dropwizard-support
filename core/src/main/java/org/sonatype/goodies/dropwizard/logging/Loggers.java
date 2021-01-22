46199eadafc1c2b3857652799d0c49e2d219c342
Jason Dillon <jdillon@users.noreply.github.com>
2021-01-21 22:25:13 -0800

    Add generic type to Class type

--- core/src/main/java/org/sonatype/goodies/dropwizard/logging/Loggers.java
+++ core/src/main/java/org/sonatype/goodies/dropwizard/logging/Loggers.java
@@ -35,7 +35,7 @@ public final class Loggers
   /**
    * Helper to create a logger and deal with class-names created by AOP platforms.
    */
-  public static Logger getLogger(final Class type) {
+  public static Logger getLogger(final Class<?> type) {
     checkNotNull(type);
     return LoggerFactory.getLogger(GuiceEnhanced.dereference(type));
   }
